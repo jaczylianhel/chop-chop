8f810c4da66e60b78c46ee9d798e7c829decd79e
jaczylianhel <jaczylianhel@gmail.com>
2017-11-09 20:02:31 +0100

    second_day

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
         <div class="session_button">
 
             <?php 
-            if(isset($_GET['text_c'])){
+            if(isset($_GET['text_c'])&&(isset($_GET['text']))) { //sprawdza czy sa wyslane dane metoda post i jest zarezerwowana pamiec dla tych danych
  echo '<h1 style="color:'.$_GET['text_c'].'">'.$_GET['text'].'</h1>'; // zmienne text_C - zmiana koloru  text - zmiana tekstu
             }else{ echo '<h1>This is just a sample page</h1>';}
             
@@ -48,17 +48,17 @@
             <tbody id='ips'>
                 <tr>
                     <td>
-                        <img src="logo_greengo.jpg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.
+                        <img src="example.jpg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.
                         <p class="clear"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur </p>
                         <p class="hide1">orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
                     </td>
                     <td>
-                        <img src="logo_greengo.jpg"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.
+                        <img src="example.jpg"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.
                         <p class="clear"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                         <p class="hide2">orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
                     </td>
                     <td>
-                        <img src="logo_greengo.jpg"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.
+                        <img src="example.jpg"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.
                         <p class="clear"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur </p>
                         <p class="hide3">orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
                     </td>
@@ -68,6 +68,7 @@
                     <td class='read2'>-read more</td>
                     <td class='read3'>-read more</td>
                 </tr>
+
             </tbody>
         </table>
     </section>
@@ -75,9 +76,9 @@
         <h1>Contact us</h1>
         <form id="form" method="get" action="post.php">
             <div class="put-left">
-                <input class="left" type="text" name="name" id="name" value="name" /><br>
-                <input class="left" type="text" name="email" id="email" value="email" /></br>
-                <input class="left" type="text" name="phone" id="phone" value="phone" /><br>
+                <input class="left" type="text" name="name" id="name" placeholder="name" /><br>
+                <input class="left" type="text" name="email" id="email" placeholder="email" /></br>
+                <input class="left" type="text" name="phone" id="phone" placeholder="phone" /><br>
             </div>
             <div class="put-right">
                 <textarea name="message" placeholder="Napisz tu wiadomość"></textarea>
@@ -95,12 +96,12 @@
     <footer>
 
         <div class='first foot'>
-            <a href="#">-home</a>
-            <a href="#">-products</a>
+            <a href="#">—home</a>
+            <a href="#">—products</a>
         </div>
         <div class="second foot">
-            <a href="#">-about Us</a><br>
-            <a href="#">-contact</a>
+            <a href="#">—about Us</a><br>
+            <a href="#">—contact</a>
         </div>
 
         <div class="third foot">
@@ -115,20 +116,19 @@
     <script>
         $(document).ready(function() {
             $('#scroll').click(function() {
-                $('body, html').animate({
-                    scrollTop: $('body').offset().top
-                }, 1000);
+                $('body, html').animate({ //pobierz naglowek body
+                    scrollTop: $('body').offset().top //idz do gory do tagu body
+                }, 1000); //czas trwania animacji
             }); //koniec funkcji scroll
-
             $('.hide1').hide();
             $('.hide2').hide();
             $('.hide3').hide();
             $('.read1').click(function() {
-                var $answer = $('.hide1');
-                if ($answer.is(':hidden')) {
-                    $answer.slideDown();
+                var $answer = $('.hide1'); // pobiera znacznik z ukrytym tekstem
+                if ($answer.is(':hidden')) { // jezeli jest ukryty
+                    $answer.slideDown(); //pokaz metoda slidedown
                 } else {
-                    $answer.fadeOut();
+                    $answer.slideUp(); //ukryj metoda slideUp
                 }
             });
             $('.read2').click(function() {
@@ -136,7 +136,7 @@
                 if ($answer.is(':hidden')) {
                     $answer.slideDown();
                 } else {
-                    $answer.fadeOut();
+                    $answer.slideUp();
                 }
             });
             $('.read3').click(function() {
@@ -144,24 +144,24 @@
                 if ($answer.is(':hidden')) {
                     $answer.slideDown();
                 } else {
-                    $answer.fadeOut();
+                    $answer.slideUp();
                 }
             });
             $('.submit').click(function() {
-                $('.contact').toggleClass("contact2");
+                $('.contact').toggleClass("contact2"); //zmienia klase na inną z wlasciwoscia display -none
                 console.log('click');
                 // return false;
             });
             $('#ipsum').hover(function() {
-                $('header nav a').toggleClass('active');
+                $('header nav a').toggleClass('active'); //zmienia wszystke znaczniki a w naglowku nav na klase active
             })
             $('#form').submit(function() {
 
-                var dane = $(this).serialize();
-                $.post('post.php', dane, processdata);
+                var dane = $(this).serialize(); //tworzy łańcuch znaków z zapytaniem
+                $.post('post.php', dane, processdata); //wysyla dane i oczekuje odpowiedzi (processdata)
 
                 function processdata(data) {
-                    if (data) {
+                    if (data) { //sprawdza, co odebrał i wykonuje polecenia (w php wysylam true albo false)
                         $('#info').html('<center><p>Dziękujemy za wysłanie formularza z następującymi danymi</p></center>');
                     } else {
                         $('#info').html('<center><p>Brak danych do wyświetlenia</p></center>')
@@ -170,22 +170,30 @@
                 return false;
             });
             $("#ipsum").click(function() {
-                const word = 'ipsum';
-                var t = document.getElementById('ips');
-                var tag = t.getElementsByTagName('td').length;
-                for (var j = 0; j < tag; j++) {
-                    var td = t.getElementsByTagName('td')[j].innerHTML;
-                    var arr = td.split(" ")
-                    var length = arr.length;
-                    for (var x = 0; x < length; x++) {
-                        if (arr[x].toLowerCase() == word) {
-                            arr.splice(x, 1, '<span class="red">' + word + '</span>');
+                const word = 'ipsum'; //wpisujemy wyraz do wyszukania
+                var t = document.getElementById('ips'); //pobieramy selektor z miejsca, którego przeszukujemy czyli tabela z artykulem
+                var tag = t.getElementsByTagName('td').length; //pobieramy liczbe artykulow (znacznik td) 
+                for (var j = 0; j < tag; j++) { // dla każdego artykulu 
+                    var td = t.getElementsByTagName('td')[j].innerHTML; //pobieramy artykul (znacznik td)
+                    var arr = td.split(" ") //robimy z niego tablice
+                    var length = arr.length; //liczymy liczbe elementow w tej tablicy
+                    for (var x = 0; x < length; x++) { //dla kazdego elementu tablicy wykonaj
+                        if (arr[x].toLowerCase() == word) { //sprawdz czy wyraz w tablicy zawiera szukany wyraz
+                            arr.splice(x, 1, '<span class="red">' + word + '</span>'); //hezeli zawiera to dopisz do niego span
                         }
                     }
-                    var arrNew = arr.join(" ");
-                    t.getElementsByTagName('td')[j].innerHTML = arrNew;
+                    var arrNew = arr.join(" "); //wszystkie elementy w tablicy scal
+                    t.getElementsByTagName('td')[j].innerHTML = arrNew; //podmien scalona tablice 
+                }
+                setTimeout(bringDefaultClassBack, 3000); //ustaw funkcje ktora bedzie dzialala 3 sekundy
+
+                function bringDefaultClassBack() {
+                    $('.red').toggleClass("black"); // po 3 sekundach zamien klase red na black 
                 }
             });
+
+
+
         });
 
     </script>
--- post.php
+++ post.php
@@ -2,14 +2,14 @@
  
     
 if(empty($_POST['name'])||empty($_POST['email'])||empty($_POST['phone'])){
-
-// print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
     echo false;
-  
 } 
     else{   
- // print "<meta http-equiv=\"refresh\" content=\"0;URL=potwierdzenie.html\">";
  echo true;
+        showFormData();
+$newName=$_POST['name'];
+$newSurname=$_POST['email'];
+$newGender=$_POST['phone'];
 } 
-    
+
       ?>
